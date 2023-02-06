757250ea913e5b97774976ce26de10080ce76ee5
shterybai <andrewkennan2000@gmail.com>
2023-02-06 16:36:45 +0000

    Added runtime markers for evaluating performance

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import gensim
 from gensim.models import Word2Vec
 from statemachine import StateMachine, State
 import requests
+import time
 
 BANNED_CHARACTERS = "\"!@#$%^&.`*()-+?_=,<>/123456789\'"
 EMPTY_WORD = "___________"
@@ -10,15 +11,12 @@ EMPTY_WORD = "___________"
 
 
 def word2vec(user_words: list[str]):
-    # print(model.most_similar(positive=words, topn=5000))
-
-    # sim_list = model.most_similar(positive=user_words, topn=100000)
-
-    sim_list = requests.get('http://744f-109-255-34-132.ngrok.io/request/?user_words=' + words)
+    print(time.time() - start_time, "seconds: Retrieving sim_list")
+    sim_list = requests.get('http://0dc5-109-255-34-132.ngrok.io/request/?user_words=' + words)
 
     word_list = [i[0] for i in sim_list.json()]
 
-    # print(word_list)
+    print(time.time() - start_time, "seconds: sim_list successfully retrieved")
 
     dictionaries(word_list)
 
@@ -31,6 +29,7 @@ def dictionaries(word_list):
     four_char_words = []
 
     # Populate word lists
+    print(time.time() - start_time, "seconds: Populating word lists...")
     for word in word_list:
         if len(word) == 11:
             if all(c not in BANNED_CHARACTERS for c in word):
@@ -45,11 +44,14 @@ def dictionaries(word_list):
             if all(c not in BANNED_CHARACTERS for c in word):
                 four_char_words.append(word)
 
+    print(time.time() - start_time, "seconds: Done")
+
     # print("Eleven character words: " + eleven_char_words[0])
     # print("Ten character words: " + ten_char_words[0])
     # print("Six character words: " + six_char_words[0])
     # print("Four character words: " + four_char_words[0])
 
+    print(time.time() - start_time, "seconds: Beginning insertions...")
     insertions(eleven_char_words, ten_char_words, six_char_words, four_char_words)
 
 
@@ -95,9 +97,12 @@ def insertions(eleven_char_words, ten_char_words, six_char_words, four_char_word
     print("14-down = " + d14)
     print("16-across = " + a16)
 
+    print("Finished. Total execution time: ", time.time() - start_time)
+
 
 # D3 assignment
 def d3_insertion(d3_banned, eleven_char_words):
+    print(time.time() - start_time, "seconds: D3 assignment")
     for word in eleven_char_words:
         if word not in d3_banned:
             d3 = word
@@ -107,6 +112,7 @@ def d3_insertion(d3_banned, eleven_char_words):
 
 # A8 assignment
 def a8_insertion(a8_banned, d3, ten_char_words):
+    print(time.time() - start_time, "seconds: A8 assignment")
     for word in ten_char_words:
         if word[4] == d3[3] and word not in a8_banned:
             a8 = word
@@ -116,6 +122,7 @@ def a8_insertion(a8_banned, d3, ten_char_words):
 
 # A12 assignment
 def a12_insertion(a12_banned, d3, ten_char_words):
+    print(time.time() - start_time, "seconds: A12 assignment")
     for word in ten_char_words:
         if word[5] == d3[7] and word not in a12_banned:
             a12 = word
@@ -125,6 +132,7 @@ def a12_insertion(a12_banned, d3, ten_char_words):
 
 # D2 assignment
 def d2_insertion(d2_banned, a8, six_char_words):
+    print(time.time() - start_time, "seconds: D2 assignment")
     for word in six_char_words:
         if word[3] == a8[2] and word not in d2_banned:
             d2 = word
@@ -134,6 +142,7 @@ def d2_insertion(d2_banned, a8, six_char_words):
 
 # D5 assignment
 def d5_insertion(d5_banned, a8, six_char_words):
+    print(time.time() - start_time, "seconds: D5 assignment")
     for word in six_char_words:
         if word[3] == a8[8] and word not in d5_banned:
             d5 = word
@@ -143,6 +152,7 @@ def d5_insertion(d5_banned, a8, six_char_words):
 
 # D10 assignment
 def d10_insertion(d10_banned, a12, six_char_words):
+    print(time.time() - start_time, "seconds: D10 assignment")
     for word in six_char_words:
         if word[2] == a12[1] and word not in d10_banned:
             d10 = word
@@ -152,6 +162,7 @@ def d10_insertion(d10_banned, a12, six_char_words):
 
 # D11 assignment
 def d11_insertion(d11_banned, a12, six_char_words):
+    print(time.time() - start_time, "seconds: D11 assignment")
     for word in six_char_words:
         if word[2] == a12[7] and word not in d11_banned:
             d11 = word
@@ -161,6 +172,7 @@ def d11_insertion(d11_banned, a12, six_char_words):
 
 # A7 assignment
 def a7_insertion(a7_banned, d3, six_char_words):
+    print(time.time() - start_time, "seconds: A7 assignment")
     for word in six_char_words:
         if word[0] == d3[1] and word[5] == d3[9] and word not in a7_banned:
             a7 = word
@@ -170,6 +182,7 @@ def a7_insertion(a7_banned, d3, six_char_words):
 
 # A15 assignment
 def a15_insertion(a15_banned, d10, d3, six_char_words):
+    print(time.time() - start_time, "seconds: A15 assignment")
     for word in six_char_words:
         if word[1] == d10[4] and word[5] == d3[9] and word not in a15_banned:
             a15 = word
@@ -179,6 +192,7 @@ def a15_insertion(a15_banned, d10, d3, six_char_words):
 
 # D1 assignment
 def d1_insertion(d1_banned, a8, four_char_words):
+    print(time.time() - start_time, "seconds: D1 assignment")
     for word in four_char_words:
         if word[3] == a8[0] and word not in d1_banned:
             d1 = word
@@ -188,6 +202,7 @@ def d1_insertion(d1_banned, a8, four_char_words):
 
 # D4 assignment
 def d4_insertion(d4_banned, a7, a8, four_char_words):
+    print(time.time() - start_time, "seconds: D4 assignment")
     for word in four_char_words:
         if word[1] == a7[2] and word[3] == a8[6] and word not in d4_banned:
             d4 = word
@@ -197,6 +212,7 @@ def d4_insertion(d4_banned, a7, a8, four_char_words):
 
 # A6 assignment
 def a6_insertion(a6_banned, d1, d2, four_char_words):
+    print(time.time() - start_time, "seconds: A6 assignment")
     for word in four_char_words:
         if word[1] == d1[1] and word[3] == d2[1] and word not in a6_banned:
             a6 = word
@@ -206,6 +222,7 @@ def a6_insertion(a6_banned, d1, d2, four_char_words):
 
 # A9 assignment
 def a9_insertion(a9_banned, d10, d2, four_char_words):
+    print(time.time() - start_time, "seconds: A9 assignment")
     for word in four_char_words:
         if word[1] == d10[0] and word[3] == d2[5] and word not in a9_banned:
             a9 = word
@@ -215,6 +232,7 @@ def a9_insertion(a9_banned, d10, d2, four_char_words):
 
 # A11 assignment
 def a11_insertion(a11_banned, d11, d5, four_char_words):
+    print(time.time() - start_time, "seconds: A11 assignment")
     for word in four_char_words:
         if word[0] == d11[0] and word[1] == d5[5] and word not in a11_banned:
             a11 = word
@@ -224,6 +242,7 @@ def a11_insertion(a11_banned, d11, d5, four_char_words):
 
 # D13 assignment
 def d13_insertion(d13_banned, a12, four_char_words):
+    print(time.time() - start_time, "seconds: D13 assignment")
     for word in four_char_words:
         if word[0] == a12[3] and word not in d13_banned:
             d13 = word
@@ -233,6 +252,7 @@ def d13_insertion(d13_banned, a12, four_char_words):
 
 # D14 assignment
 def d14_insertion(d14_banned, a12, four_char_words):
+    print(time.time() - start_time, "seconds: D14 assignment")
     for word in four_char_words:
         if word[0] == a12[9] and word not in d14_banned:
             d14 = word
@@ -242,6 +262,7 @@ def d14_insertion(d14_banned, a12, four_char_words):
 
 # A16 assignment
 def a16_insertion(a16_banned, d11, d14, four_char_words):
+    print(time.time() - start_time, "seconds: A16 assignment")
     for word in four_char_words:
         if word[0] == d11[4] and word[2] == d14[2] and word not in a16_banned:
             a16 = word
@@ -252,6 +273,8 @@ def a16_insertion(a16_banned, d11, d14, four_char_words):
 user_word_1 = input("Enter a word: ")
 user_word_2 = input("Enter another word: ")
 user_word_3 = input("Enter a final word: ")
+
+start_time =  time.time()
 
 words = user_word_1 + "," + user_word_2 + "," + user_word_3
 word2vec(words.split(','))
